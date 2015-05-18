0241721c72870aa5d7dba50ce0ddf72becc36580
colintbowers <colintbowers@gmail.com>
2015-05-18 11:36:01 +1000

    First_Commit

--- src/ForecastEval.jl
+++ src/ForecastEval.jl
@@ -1,5 +1,33 @@
 module ForecastEval
+#-----------------------------------------------------------
+#PURPOSE
+#	Colin T. Bowers module for forecast evaluation
+#NOTES
+#LICENSE
+#	MIT License (see github repository for more detail: https://github.com/colintbowers/DependentBootstrap.jl.git)
+#-----------------------------------------------------------
 
-# package code goes here
+#Load any entire modules that are needed (use import ModuleName1, ModuleName2, etc)
+using 	Distributions,
+		StatsBase
+
+#Load any specific variables/functions that are needed (use import ModuleName1.FunctionName1, ModuleName2.FunctionName2, etc)
+import 	Base.string,
+		Base.show,
+		Base.copy
+
+#Specify the variables/functions to export (use export FunctionName1, FunctionName2, etc)
+
+
+
+#******************************************************************************
+
+#----------------------------------------------------------
+#SET CONSTANTS FOR MODULE
+#----------------------------------------------------------
+
+
+
+
 
 end # module
