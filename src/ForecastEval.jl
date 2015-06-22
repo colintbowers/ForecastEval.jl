41834c86115b0dcca8cbbaec14e2640c4fe9390b
colintbowers <colintbowers@gmail.com>
2015-06-22 18:23:15 +1000

    Fix DM

--- src/ForecastEval.jl
+++ src/ForecastEval.jl
@@ -1,15 +1,10 @@
 module ForecastEval
-
-#TO ADD
-#p-value for bootstrap input to diebold-mariano after building SortedVector type
-
-
 #-----------------------------------------------------------
 #PURPOSE
 #	Colin T. Bowers module for forecast evaluation
 #NOTES
 #LICENSE
-#	MIT License (see github repository for more detail: https://github.com/colintbowers/DependentBootstrap.jl.git)
+#	MIT License (see github repository for more detail: https://github.com/colintbowers/ForecastEval.jl.git)
 #-----------------------------------------------------------
 
 #Load any entire modules that are needed (use import ModuleName1, ModuleName2, etc)
@@ -23,7 +18,10 @@ import 	Base.string,
 		Base.show
 
 #Specify the variables/functions to export (use export FunctionName1, FunctionName2, etc)
-
+export 	dieboldMariano,
+		DMMethod,
+		DMAsymptotic,
+		DMBootstrap
 
 
 #******************************************************************************
@@ -100,66 +98,87 @@ show(d::DMMethod) = show(STDOut, d)
 #	Diebold, Francis X., Mariano, Roberto S. (1995) "Comparing Predictive Accuracy", Journal of Business and Economic Statistics, 13 (3), pp. 253-263
 #----------------------------------------------------------
 #Diebold-Mariano test using asymptotic approximation (see section 1.1 of their paper)
-function dieboldMariano{T<:Number}(lossDiff::Vector{T}, method::DMAsymptotic, confLevel::Number=0.05)
+function dieboldMariano{T<:Number}(lossDiff::Vector{T}, method::DMAsymptotic; confLevel::Number=0.05)
 	!(0.0 < confLevel < 1.0) && error("Confidence level must lie between 0 and 1")
 	length(lossDiff) < 2 && error("Loss differential series must contain at least two observations")
 	m = mean(lossDiff)
-	method.lagWindow == 0 ? v = var(lossDiff) : v = var(lossDiff) + 2 * sum(autocov(lossDiff, 1:method.lagWindow)) #Rectangular kernel to specified lag
-	v <= 0 ? v = 0 #If variance estimate is negative, bind to zero so we auto-reject the null
+	if method.lagWindow == 0
+		v = var(lossDiff)
+	else
+		v = var(lossDiff) + 2 * sum(autocov(lossDiff, 1:method.lagWindow)) #Rectangular kernel to specified lag
+	end
+	v <= 0 && v = 0 #If variance estimate is negative, bind to zero so we auto-reject the null
 	testStat = m / sqrt(v / length(lossDiff))
 	testStat < 0 ? (pVal = 2*cdf(Normal(), testStat)) : (pVal = 2*cdf(Normal(), -1*testStat)) #Get p-value
 	pVal < confLevel ? return(sign(m), pVal) : return(0, pVal) #Sign of output flag tells you which forecast was preferred
 end
 #Diebold-Mariano test using a dependent bootstrap
-function dieboldMariano{T<:Number}(lossDiff::Vector{T}, method::DMBootstrap, confLevel::Number=0.05)
+function dieboldMariano{T<:Number}(lossDiff::Vector{T}, method::DMBootstrap; confLevel::Number=0.05)
 	!(0.0 < confLevel < 1.0) && error("Confidence level must lie between 0 and 1")
 	length(lossDiff) < 2 && error("Loss differential series must contain at least two observations")
-	testStatVec = dBootstrapStatistic(lossDiff, method.bootstrapParam)
-	boundVec = quantile(testStatVec, [confLevel/2, 1 - confLevel/2])
-	(boundVec[1] < 0 < boundVec[2]) ? return(0, NaN) : return(sign(boundVec[1]), NaN) #If 0 does not lie in bounds, then both bounds will have the same sign
-end
-#Keyword method
-function dieboldMariano{T<:Number}(lossDiff::Vector{T}; method::ASCIIString="bootstrap", numResample::Int=1000, lagWindow::Int=0, confLevel::Number=0.05)
-	if method == "asymptotic"
-		return(dieboldMariano(lossDiff, DMAsymptotic(lagWindow)), confLevel)
+	getBlockLength(method.bootstrapParam) <= 0 && dBootstrapBlockLength!(method.bootstrapParam, lossDiff) #detect block-length if necessary
+	statVec = dBootstrapStatistic(lossDiff, method.bootstrapParam)
+	sort!(statVec)
+	i = searchsortedlast(statVec, 0.0)
+	NHalf = 0.5 * length(statVec)
+	if i < NHalf
+		pVal = i / NHalf
+		outSign = 1
 	else
-		method == "bootstrap" && (method = "stationary")
-		bp = BootstrapParam(lossDiff, bootstrapMethod=method, numResample=numResample, statistic="mean")
-		return(dieBoldMariano(lossDiff, DMBootstrap(bp)))
+		pVal = (length(statVec) - i) / NHalf
+		outSign = -1
 	end
+	pVal < confLevel ? return(outSign, pVal) : return(0, pVal)
 end
-#Diebold-Mariano wrapper for calculating loss differential
-function dieboldMariano{T<:Number}(xhat1::Vector{T}, xhat2::Vector{T}, x::Vector{T}, lossFunc::LossFunction, method::DMMethod, confLevel::Number=0.05)
-	!(length(xhat1) == length(xhat2) == length(x)) && error("Input data vectors must all be of equal length")
-	return(dieboldMariano(loss(xhat1, x, lossFunc) - loss(xhat2, x, lossFunc), method, confLevel))
-end
-#Keyword method wrapper
-function dieboldMariano{T1<:Number, T2<:Union(ASCIIString, LossFunction)}(xhat1::Vector{T1}, xhat2::Vector{T1}, x::Vector{T1}; lossFunc::T2=SquaredError(), method::ASCIIString="bootstrap", numResample::Int=1000, lagWindow::Int=0, confLevel::Number=0.05)
-	!(length(xhat1) == length(xhat2) == length(x)) && error("Input data vectors must all be of equal length")
-	if typeof(lossFunc) == ASCIIString
-		if lossFunc = "MSE"
-			lossFunc = SquaredError()
-		elseif lossFunc = "MAE"
-			lossFunc = AbsoluteError()
-		elseif lossFunc = "QLIKE"
-			lossFunc = QLIKE()
-		else
-			error("lossFunc string not recognised")
-		end
-	end
-	lossDiff = loss(xhat1, x, lossFunc) - loss(xhat2, x, lossFunc)
+#Keyword method (loss differential already computed)
+dieboldMariano{T<:Number}(lossDiff::Vector{T}; method::ASCIIString="bootstrap", numResample::Int=1000, lagWindow::Int=0, confLevel::Number=0.05) = dieboldMariano(lossDiff, dM_getmethod(length(lossDiff), method, numResample, lagWindow), confLevel=confLevel)
+#Wrapper (loss differential not computed)
+dieboldMariano{T<:Number}(xhat1::Vector{T}, xhat2::Vector{T}, x::Vector{T}, lossFunc::LossFunction, method::DMMethod, confLevel::Number=0.05) = dieboldMariano(dM_getlossdiff(xhat1, xhat2, x, lossFunc), method, confLevel=confLevel)
+#Keyword method wrapper (loss differential not computed)
+dieboldMariano{T1<:Number, T2<:Union(ASCIIString, LossFunction)}(xhat1::Vector{T1}, xhat2::Vector{T1}, x::Vector{T1}; lossFunc::T2=SquaredError(), method::ASCIIString="bootstrap", numResample::Int=1000, lagWindow::Int=0, confLevel::Number=0.05) = dieboldMariano(dM_getlossdiff(xhat1, xhat2, x, lossFunc), dM_getmethod(length(lossDiff), method, numResample, lagWindow), confLevel=confLevel)
+#Non-exported functions that help with keyword wrapper routines
+function dM_getmethod(numObs::Int, method::ASCIIString, numResample::Int, lagWindow::Int)
 	if method == "asymptotic"
-		return(dieboldMariano(lossDiff, DMAsymptotic(lagWindow)), confLevel)
+		methodType = DMAsymptotic(lagWindow)
+	elseif method == "bootstrap" || method == "stationary" || method == "circularBlock" || method == "movingBlock"
+		bp = BootstrapParam(numObs, bootstrapMethod=method, numResample=numResample, statistic="mean")
+		methodType = DMBootstrap(bp)
 	else
-		method == "bootstrap" && (method = "stationary")
-		bp = BootstrapParam(lossDiff, bootstrapMethod=method, numResample=numResample, statistic="mean")
-		return(dieBoldMariano(lossDiff, DMBootstrap(bp)))
+		error("Unable to infer desired method from inputs")
 	end
+	return(methodType)
+end
+function dM_getlossdiff{T<:Number}(xhat1::Vector{T}, xhat2::Vector{T}, x::Vector{T}, lossFunc::LossFunction)
+	!(length(xhat1) == length(xhat2) == length(x)) && error("Input data vectors must all be of equal length")
+	return(loss(xhat1, x, lossFunc) - loss(xhat2, x, lossFunc))
+end
+function dM_getlossdiff{T<:Number}(xhat1::Vector{T}, xhat2::Vector{T}, x::Vector{T}, lossFunc::ASCIIString)
+	if lossFunc = "MSE"
+		lossFunc = SquaredError()
+	elseif lossFunc = "MAE"
+		lossFunc = AbsoluteError()
+	elseif lossFunc = "QLIKE"
+		lossFunc = QLIKE()
+	else
+		error("lossFunc string not recognised")
+	end
+	return(dieboldMariano_getlossdiff(xhat1, xhat2, x, lossFunc))
 end
 
 
 
 
+#----------------------------------------------------------
+#FUNCTION
+#	realityCheck
+#INPUT
+#OUTPUT
+#PURPOSE
+#	Perform a White (2000) reality check for determining if a set of forecast models outperform a benchmark
+#NOTES
+#REFERENCES
+#	White (2000) "A Reality Check for Data Snooping", Econometrica, 68 (5), pp. 1097-1126
+#----------------------------------------------------------
 
 
 
