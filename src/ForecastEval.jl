4edb29ab4c6d3dc7b98b46593643b229df257650
colintbowers <colintbowers@gmail.com>
2015-06-26 18:23:12 +1000

    Major update

--- src/ForecastEval.jl
+++ src/ForecastEval.jl
@@ -18,7 +18,7 @@ import 	Base.string,
 		Base.show
 
 #Specify the variables/functions to export (use export FunctionName1, FunctionName2, etc)
-export 	dieboldMariano,
+export 	dieboldmariano,
 		DMMethod,
 		DMAsymptotic,
 		DMBootstrap
@@ -29,25 +29,21 @@ export 	dieboldMariano,
 #----------------------------------------------------------
 #SET CONSTANTS FOR MODULE
 #----------------------------------------------------------
-
-
-
+#Currently none
 
 
 #----------------------------------------------------------
-#TYPE
-#	DMStandard
-#	DMBootstrap
-#FIELDS
-#	Fields store parameteres necessary to perform a Diebold-Mariano test using the indicated method
-#METHODS
-#PURPOSE
-#	These types are used to call different methods of dieboldMariano, depending on how the user wants to perform the test.
+#ROUTINE
+#	Diebold-Mariano
+#OUTPUT
+#	The tuple (pval, tailregion), where pval::Float64 is the p-value from the test, and tailregion::Int takes values 0 (fail to reject), 1 (reject in favour of forecast 1), or -1 (reject in favour of forecast 2)
+#REFERENCES
+#	Diebold, Francis X., Mariano, Roberto S. (1995) "Comparing Predictive Accuracy", Journal of Business and Economic Statistics, 13 (3), pp. 253-263
 #NOTES
+#	You can skip the block length selection procedure by specifying a block-length > 0
 #----------------------------------------------------------
-#Abstract type for nesting method types for diebold mariano test
+#-------- METHOD TYPES ---------
 abstract DMMethod
-#Type indicating to do the asymptotic test (section 1.1 of their paper)
 type DMAsymptotic <: DMMethod
 	lagWindow::Int
 	function DMAsymptotic(lagWindow::Int)
@@ -56,22 +52,17 @@ type DMAsymptotic <: DMMethod
 	end
 end
 DMAsymptotic() = DMAsymptotic(0)
-#Type indicating to use a bootstrap to conduct the test
 type DMBootstrap <: DMMethod
 	bootstrapParam::BootstrapParam
 	function DMBootstrap(bootstrapParam::BootstrapParam)
-		if bootstrapParam.statistic != "mean" && bootstrapParam.statistic != mean
-			error("statistic field in BootstrapParam must be set to mean")
-		end
-		new(bootstrapParam)
+		bootstrapParam.statistic == mean ? new(bootstrapParam) : error("statistic field in BootstrapParam must be set to mean")
 	end
 end
-DMBootstrap{T<:Number}(lossDiff::Vector{T}) = DMBootstrap(BootstrapParam(lossDiff, bootstrapMethod="stationary", statistic="mean"))
-#------- METHODS --------------
-#string methods
+DMBootstrap(numObs::Int) = DMBootstrap(BootstrapParam(numObs, statistic=mean))
+DMBootstrap{T<:Number}(x::Vector{T}) = DMBoostrap(BootstrapParap(x, statistic=mean))
+#type methods
 Base.string(d::DMAsymptotic) = "dmAsymptotic"
 Base.string(d::DMBootstrap) = "dmBootstrap"
-#show methods
 function Base.show(io::IO, d::DMAsymptotic)
 	println(io, "Diebold-Mariano Asymptotic Method:")
 	pritnln(io, "    Lag window = " * string(d.lagWindow))
@@ -80,21 +71,10 @@ function Base.show(io::IO, d::DMBootstrap)
 	println(io, "Diebold-Mariano Bootstrap Method:")
 	show(io, d.bootstrapParam)
 end
-#show wrapper on STDOUT
 Base.show(d::DMMethod) = show(STDOut, d)
-#----------------------------------------------------------
-#FUNCTION
-#	dieboldMariano
-#INPUT
-#OUTPUT
-#PURPOSE
-#	Perform a Diebold-Mariano (1995) test for determining if there is a difference in loss between two forecasting models
-#NOTES
-#REFERENCES
-#	Diebold, Francis X., Mariano, Roberto S. (1995) "Comparing Predictive Accuracy", Journal of Business and Economic Statistics, 13 (3), pp. 253-263
-#----------------------------------------------------------
+#--------- FUNCTION -----------------------
 #Diebold-Mariano test using asymptotic approximation (see section 1.1 of their paper)
-function dieboldMariano{T<:Number}(lossDiff::Vector{T}, method::DMAsymptotic; confLevel::Number=0.05)
+function dieboldmariano{T<:Number}(lossDiff::Vector{T}, method::DMAsymptotic; confLevel::Number=0.05)
 	!(0.0 < confLevel < 1.0) && error("Confidence level must lie between 0 and 1")
 	length(lossDiff) < 2 && error("Loss differential series must contain at least two observations")
 	m = mean(lossDiff)
@@ -103,127 +83,100 @@ function dieboldMariano{T<:Number}(lossDiff::Vector{T}, method::DMAsymptotic; co
 	else
 		v = var(lossDiff) + 2 * sum(autocov(lossDiff, 1:method.lagWindow)) #Rectangular kernel to specified lag
 	end
-	v <= 0 && v = 0 #If variance estimate is negative, bind to zero so we auto-reject the null
+	v <= 0 && (v = 0) #If variance estimate is negative, bind to zero so we auto-reject the null
 	testStat = m / sqrt(v / length(lossDiff))
-	testStat < 0 ? (pVal = 2*cdf(Normal(), testStat)) : (pVal = 2*cdf(Normal(), -1*testStat)) #Get p-value
-	pVal < confLevel ? return(sign(m), pVal) : return(0, pVal) #Sign of output flag tells you which forecast was preferred
+	return(pval(Normal(), testStat, twoSided=true))
 end
 #Diebold-Mariano test using a dependent bootstrap
-function dieboldMariano{T<:Number}(lossDiff::Vector{T}, method::DMBootstrap; confLevel::Number=0.05)
+function dieboldmariano{T<:Number}(lossDiff::Vector{T}, method::DMBootstrap; confLevel::Number=0.05)
 	!(0.0 < confLevel < 1.0) && error("Confidence level must lie between 0 and 1")
 	length(lossDiff) < 2 && error("Loss differential series must contain at least two observations")
-	getBlockLength(method.bootstrapParam) <= 0 && dBootstrapBlockLength!(method.bootstrapParam, lossDiff) #detect block-length if necessary
-	method.bootstrapParam.statistic != "mean" && error("statistic field in BootstrapParam must be set to mean")
-	statVec = dBootstrapStatistic(lossDiff, method.bootstrapParam)
+	getBlockLength(method.bootstrapParam) <= 0 && dbootstrapblocklength!(method.bootstrapParam, lossDiff) #detect block-length if necessary
+	method.bootstrapParam.statistic != mean && error("statistic field in BootstrapParam must be set to mean")
+	testStat = mean(lossDiff)
+	statVec = dbootstrapstatistic(lossDiff, method.bootstrapParam) - testStat #Centre statVec on zero
 	sort!(statVec)
-	i = searchsortedlast(statVec, 0.0)
-	NHalf = 0.5 * length(statVec)
-	if i < NHalf
-		pVal = i / NHalf
-		outSign = 1
-	else
-		pVal = (length(statVec) - i) / NHalf
-		outSign = -1
-	end
-	pVal < confLevel ? return(outSign, pVal) : return(0, pVal)
+	return(pval(statVec, testStat))
 end
-#Keyword method (loss differential already computed)
-dieboldMariano{T<:Number}(lossDiff::Vector{T}; method::ASCIIString="bootstrap", numResample::Int=1000, lagWindow::Int=0, confLevel::Number=0.05) = dieboldMariano(lossDiff, dM_getmethod(length(lossDiff), method, numResample, lagWindow), confLevel=confLevel)
-#Wrapper (loss differential not computed)
-dieboldMariano{T<:Number}(xhat1::Vector{T}, xhat2::Vector{T}, x::Vector{T}, lossFunc::LossFunction, method::DMMethod, confLevel::Number=0.05) = dieboldMariano(dM_getlossdiff(xhat1, xhat2, x, lossFunc), method, confLevel=confLevel)
-#Keyword method wrapper (loss differential not computed)
-dieboldMariano{T1<:Number, T2<:Union(ASCIIString, LossFunction)}(xhat1::Vector{T1}, xhat2::Vector{T1}, x::Vector{T1}; lossFunc::T2=SquaredError(), method::ASCIIString="bootstrap", numResample::Int=1000, lagWindow::Int=0, confLevel::Number=0.05) = dieboldMariano(dM_getlossdiff(xhat1, xhat2, x, lossFunc), dM_getmethod(length(lossDiff), method, numResample, lagWindow), confLevel=confLevel)
-#Non-exported functions that help with keyword wrapper routines
-function dM_getmethod(numObs::Int, method::ASCIIString, numResample::Int, lagWindow::Int)
-	if method == "asymptotic"
-		methodType = DMAsymptotic(lagWindow)
-	elseif method == "bootstrap" || method == "stationary" || method == "circularBlock" || method == "movingBlock"
-		bp = BootstrapParam(numObs, bootstrapMethod=method, numResample=numResample, statistic="mean")
-		methodType = DMBootstrap(bp)
-	else
-		error("Unable to infer desired method from inputs")
-	end
-	return(methodType)
+#Keyword wrapper
+function dieboldmariano{T1<:Number, T2<:Number}(lossDiff::Vector{T1}; method::DMMethod=DMBootstrap(length(lossDiff)), numResample::Int=1000, blockLength::T2=-1, lagWindow::Int=0, confLevel::Number=0.05)
+	typeof(method) == DMBootstrap && update!(method.bootstrapParam, numResample=numResample, blockLength=blockLength)
+	typeof(method) == DMAsymptotic && (method.lagWindow=lagWindow)
+	return(dieboldmariano(lossDiff, method, confLevel=confLevel))
 end
-function dM_getlossdiff{T<:Number}(xhat1::Vector{T}, xhat2::Vector{T}, x::Vector{T}, lossFunc::LossFunction)
-	!(length(xhat1) == length(xhat2) == length(x)) && error("Input data vectors must all be of equal length")
-	return(loss(xhat1, x, lossFunc) - loss(xhat2, x, lossFunc))
-end
-function dM_getlossdiff{T<:Number}(xhat1::Vector{T}, xhat2::Vector{T}, x::Vector{T}, lossFunc::ASCIIString)
-	if lossFunc = "MSE"
-		lossFunc = SquaredError()
-	elseif lossFunc = "MAE"
-		lossFunc = AbsoluteError()
-	elseif lossFunc = "QLIKE"
-		lossFunc = QLIKE()
-	else
-		error("lossFunc string not recognised")
-	end
-	return(dieboldMariano_getlossdiff(xhat1, xhat2, x, lossFunc))
-end
+#Wrapper for calculating loss differential
+dieboldmariano{T1<:Number, T2<:Number}(xhat1::Vector{T1}, xhat2::Vector{T1}, x::Vector{T1}, lossFunc::LossFunction=SquaredError(); method::DMMethod=DMBootstrap(length(x)), numResample::Int=1000, blockLength::T2=-1, lagWindow::Int=0, confLevel::Number=0.05) = dieboldmariano(lossdiff(xhat1, xhat2, x, lossFunc), method=method, numResample=numResample, blockLength=blockLength, lagWindow=lagWindow, confLevel=confLevel)
+
+
 
 
 
 #----------------------------------------------------------
-#TYPE
-#	RCBootstrap
-#FIELDS
-#	Fields store parameteres necessary to perform a Reality-Check test using the indicated method
-#METHODS
-#PURPOSE
-#	These types are used to call different methods of realityCheck, depending on how the user wants to perform the test.
+#ROUTINE
+#	Reality Check
+#OUTPUT
+#	Returns a p-value vector, with one p-value for each iteration of the loop over models. Usually it is the last p-value that is of interest.
+#REFERENCES
+#	White (2000) "A Reality Check for Data Snooping", Econometrica, 68 (5), pp. 1097-1126
 #NOTES
+#	You can skip the block length selection procedure by specifying a block-length > 0
 #----------------------------------------------------------
-type RCBootstrap
+#-------- METHOD TYPES ---------
+abstract RCMethod
+type RCBootstrap <: RCMethod
 	bootstrapParam::BootstrapParam
-	blockLengthFilter::ASCIIString
-	function RCBootstrap(bootstrapParam::BootstrapParam, blockLengthFilter::ASCIIString)
-		!(blockLengthFilter == "mean" || blockLengthFilter == "median" || blockLengthFilter == "maximum" || blockLengthFilter == "first") && error("Invalid value for blockLengthFilter")
+	blockLengthFilter::Symbol
+	function RCBootstrap(bootstrapParam::BootstrapParam, blockLengthFilter::Symbol)
+		!(blockLengthFilter == :mean || blockLengthFilter == :median || blockLengthFilter == :maximum || blockLengthFilter == :first) && error("Invalid value for blockLengthFilter")
 		new(bootstrapParam, blockLengthFilter)
 	end
 end
+type RCBootstrapAlt <: RCMethod
+	bootstrapParam::BootstrapParam
+	blockLengthFilter::Symbol
+	function RCBootstrapAlt(bootstrapParam::BootstrapParam, blockLengthFilter::Symbol)
+		!(blockLengthFilter == :mean || blockLengthFilter == :median || blockLengthFilter == :maximum || blockLengthFilter == :first) && error("Invalid value for blockLengthFilter")
+		new(bootstrapParam, blockLengthFilter)
+	end
+end
+#type methods
 Base.string(x::RCBootstrap) = "rcBootstrap"
 function Base.show(io::IO, x::RCBootstrap)
 	println(io, "Reality check via bootstrap. Bootstrap parameters are:")
 	show(io, x.bootstrapParam)
 end
 Base.show(x::RCBootstrap) = show(STDOUT, x)
-#----------------------------------------------------------
-#FUNCTION
-#	realityCheck
-#INPUT
-#OUTPUT
-#PURPOSE
-#	Perform a White (2000) reality check for determining if a set of forecast models outperform a benchmark
-#NOTES
-#REFERENCES
-#	White (2000) "A Reality Check for Data Snooping", Econometrica, 68 (5), pp. 1097-1126
-#----------------------------------------------------------
-function realityCheck{T<:Number}(lD::Matrix{T}, method::RCBootstrap)
+#-------- FUNCTION -------------
+#realitycheck for bootstrap method
+function realitycheck{T<:Number}(lD::Matrix{T}, method::RCBootstrap)
 	numObs = size(lD, 1)
 	numModel = size(lD, 2)
 	rootNumObs = sqrt(numObs)
 	numObs < 3 && error("Not enough observations to perform a reality check")
 	numModel < 1 && error("Input data matrix is empty")
-	realityCheck_blocklength!(lD, method) #Gets appropriate block-length (if needed)
+	rC_blocklength!(lD, method) #Gets appropriate block-length (if needed)
 	method.bootstraParam.numObsData != numObs && error("numObsData field in BootstrapParam is not equal to number of rows in loss differential matrix")
-	typeof(method.bootstrapParam.bootstrapMethod) == TaperedBlock && error("realityCheck currently not possible using tapered block bootstrap")
-	inds = dBootstrapIndex(method.bootstrapParam)
+	typeof(method.bootstrapParam.bootstrapMethod) == TaperedBlock && error("realitycheck currently not possible using tapered block bootstrap")
+	inds = dbootstrapindex(method.bootstrapParam)
 	fBoot = Array(Float64, size(inds, 2))
 	vBoot = Array(Float64, size(inds, 2))
 	vBoot *= -Inf #All -Inf values will get updated on first iteration
 	pValVec = Array(Float64, numModel)
-	vBarOld = -Inf
+	vBarOld = -Inf #-Inf gets updated on first iteration
 	for k = 1:numModel
 		fBar = mean(sub(lD, 1:numObs, k))
 		vBar = max(vBarOld, rootNumObs * fBar)
-		realityCheck_updateboot!(lD, inds, numObs, rootNumObs, k, fBar, fBoot, vBoot)
-		#DO TEST AND GET PVALUE HERE
+		rC_updateboot!(lD, inds, numObs, rootNumObs, k, fBar, fBoot, vBoot)
+		println(vBoot) #investigate QuickSort
+		sort!(vBoot) #Consider specifying insertion sort (default is QuickSort) since vBar is likely to be close to sorted
+		M = searchsortedlast(vBoot, vBar)
+		pValVec[k] = 1 - (M / numObs)
+		vBarOld = vBar
 	end
-
-	#FINISH UP HERE
+	return(pValVec)
 end
-function realityCheck_updateboot!{T<:Number}(lD::Matrix{T}, inds::Matrix{Int}, numObs::Int, rootNumObs::Float64, modelNum::Int, fBar::Float64, fBoot::Vector{Float64}, vBoot::Vector{Float64})
+#Non-exported function used to update the \bar{V}_k and \bar{V}_{k,i}^* in White (2000) (see page 1110)
+function rC_updateboot!{T<:Number}(lD::Matrix{T}, inds::Matrix{Int}, numObs::Int, rootNumObs::Float64, modelNum::Int, fBar::Float64, fBoot::Vector{Float64}, vBoot::Vector{Float64})
 	lDSub = sub(lD, 1:numObs, modelNum)
 	for i = 1:size(inds, 2)
 		fBoot[i] = mean(lDSub[sub(inds, 1:numObs, i)])
@@ -231,24 +184,17 @@ function realityCheck_updateboot!{T<:Number}(lD::Matrix{T}, inds::Matrix{Int}, n
 	end
 	return(true)
 end
-
-
-
-
-function realityCheck_blocklength!{T<:Number}(lD::Matrix{T}, method::RCBootstrap)
+#Non-exported function used to choose a block length for bootstrap. Note, if method.bootstrapParam is already > 0, then this routine is skipped.
+function rC_blocklength!{T<:Number}(lD::Matrix{T}, method::RCBootstrap)
 	if getBlockLength(method.bootstrapParam) <= 0
-		if blockLengthFilter == "first"
-			bL = dBootstrapBlockLength(lD[:, 1], method.bootstrapParam)
+		if blockLengthFilter == :first
+			bL = dbootstrapblocklength(lD[:, 1], method.bootstrapParam)
 		else
-			blockLengthVec = [ dBootstrapBlockLength(lD[:, n], method.bootstrapParam) for n = 1:size(lD, 2) ]
-			if blockLengthFilter == "mean"
-				bL = mean(blockLengthVec)
-			elseif blockLengthFilter == "median"
-				bL = median(blockLengthVec)
-			elseif blockLengthFilter == "maximum"
-				bL = max(blockLengthVec)
-			else
-				error("Invalid value for blockLengthFilter")
+			blockLengthVec = [ dbootstrapblocklength(lD[:, n], method.bootstrapParam) for n = 1:size(lD, 2) ]
+			if blockLengthFilter == :mean;	bL = mean(blockLengthVec)
+			elseif blockLengthFilter == :median; bL = median(blockLengthVec)
+			elseif blockLengthFilter == :maximum; bL = max(blockLengthVec)
+			else; error("Invalid value for blockLengthFilter")
 			end
 		end
 		replaceBlockLength!(method.bootstrapParam, bL)
@@ -257,6 +203,99 @@ function realityCheck_blocklength!{T<:Number}(lD::Matrix{T}, method::RCBootstrap
 	end
 	return(bL)
 end
+#Alternative bootstrap methodology (duplicated my MatLab code)
+function realitycheck{T<:Number}(lD::Matrix{T}, method::RCBootstrapAlt)
+	numObs = size(lD, 1)
+	numModel = size(lD, 2)
+	rootNumObs = sqrt(numObs)
+	numResample = method.bootstrapParam.numResample
+	numObs < 3 && error("Not enough observations to perform a reality check")
+	numModel < 1 && error("Input data matrix is empty")
+	rC_blocklength!(lD, method) #Gets appropriate block-length (if needed)
+	method.bootstraParam.numObsData != numObs && error("numObsData field in BootstrapParam is not equal to number of rows in loss differential matrix")
+	typeof(method.bootstrapParam.bootstrapMethod) == TaperedBlock && error("realitycheck currently not possible using tapered block bootstrap")
+	inds = dbootstrapindex(method.bootstrapParam)
+	mld = [ mean(lD[:, k]) k = 1:numModel ]
+	mldBoot = [ mean(sub(lD, 1:numObs, k)[sub(inds, 1:numObs, i)]) for i = 1:numResample, k = 1:numModel ]
+	v = maximum(rootNumObs * mldBoot, 1)
+	vBoot = maximum(rootNumObs * (mldBoot .- mld), 2)
+	pVal = sum(vBoot .> v) / numResample
+	return([pVal])
+end
+#Keyword wrapper
+function realitycheck{T<:Number}(lD::Matrix{T}; method::RCMethod=RCBootstrap(), numResample::Int=1000, blockLength::Number=-1, blockLengthFilter::ASCIIString="mean")
+	update!(method.bootstrapParam, numResample=numResample, blockLength=blockLength)
+	method.blockLengthFilter = blockLengthFilter
+	return(realitycheck(lD, methodIn))
+end
+#Keyword wrapper (calculates loss differential)
+realitycheck{T<:Number}(xPrediction::Matrix{T}, xBaseCase::Vector{T}, x::Vector{T}, lossFunc::LossFunction=SquaredError(); method::RCMethod=RCBootstrap(), numResample::Int=1000, blockLength::Number=-1, blockLengthFilter::ASCIIString="mean") = realitycheck(lossdiff(xPrediction, xBaseCase, xTrue, lossFunc), method=method, numResample=numResample, blockLength=blockLength, blockLengthFilter=blockLengthFilter)
+
+
+
+
+
+
+#----------------------------------------------------------
+#ROUTINE
+#	SPA Test
+#OUTPUT
+#REFERENCES
+#	Hansen (2005) "A Test for Superior Predictive Ability", Journal of Business and Economic Statistics, 23 (4), pp. 365-380
+#NOTES
+#	You can skip the block length selection procedure by specifying a block-length > 0
+#----------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Function for getting p-values from one-sided or two-sided statistical tests. Input can be eiter sorted vector of iid draws from relevant distribution, or an explicit distribution
+function pval{T<:Number}(xVec::Vector{T}, xObs::T; twoSided::Bool=true)
+	if twoSided
+		i = searchsortedlast(xVec, xObs)
+		NHalf = 0.5 * length(xVec)
+		if i <= NHalf
+			tailRegion = -1
+			pv = i / NHalf
+		else
+			tailRegion = 1
+			pv = 2 - (i / NHalf)
+		end
+	else
+		tailRegion = 1
+		pv = 1 - (searchsortedlast(xVec, xObs) / length(xVec))
+	end
+	return(pv, tailRegion)
+end
+function pval{T<:Number}(d::Distribution, xObs::T; twoSided::Bool=true)
+	if twoSided
+		if xObs < mean(d)
+			tailRegion = -1
+			pv = 2*cdf(d, xObs)
+		else
+			tailRegion = 1
+			pv = 2*cdf(d, -1 * xObs)
+		end
+	else
+		tailRegion = 1
+		pv = cdf(d, xObs)
+	end
+	return(pv, tailRegion)
+end
+
 
 
 
